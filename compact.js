5eb6650d8fd47030cb392891b973b2ad6692ca19
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-20 14:12:41 +0200

    make createSST a stream

--- compact.js
+++ compact.js
@@ -6,13 +6,15 @@ function sizes (tables) {
   return tables.map(function (e) { return { level: e.level, size: e.size, type: e.type} })
 }
 
-module.exports = function (tables, createSST, cb) {
-  var tables = tables.slice()
+module.exports = function (tables, sstStream) {
   var total = tables.reduce(function (a, e) { return a + e.size }, 0)
+
+  console.log('pre', sizes(tables))
 
   var memtable = tables.shift()
   var stream = memtable.createReadStream(), level = 1, levels = [1]
   var target = 100, level = 0
+
 
   //estimates for the final size of the compaction.
   //if there are lots of collisions, then it's the size of the largest table
@@ -23,7 +25,7 @@ module.exports = function (tables, createSST, cb) {
   while(tables.length) {
     var table = tables[0]
     if(table.size > target * 2)
-      break //compact this table
+      break //do not compact this table
     else if(table.size <= target * 2) {
       target *= 2
       console.log('double', target)
@@ -32,19 +34,20 @@ module.exports = function (tables, createSST, cb) {
     max += table.size
     min = Math.max(min, table.size)
     tables.shift()
+    console.log(sizes(tables))
 
     stream = merge(table.createReadStream(), stream, function (a, b) {
       return a.key < b.key ? -1 : a.key > b.key ? 1 : 0
     })
   }
 
-  pull(
-    stream,
-    createSST(function (err, sst) {
-      console.log('COMPACTED', sizes([sst].concat(tables)))
+  var _tables = tables
 
-      cb(null, sst, tables)
-    })
-  )
+  console.log(sizes(_tables))
+
+  pull(stream, sstStream)
+
+
+  return _tables
 }
 
