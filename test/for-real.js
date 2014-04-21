9f77fbd671a5ca41521837751ecbaece7f93e7ed
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-21 11:38:43 +0200

    rudimentary tests

--- test/for-real.js
+++ test/for-real.js
@@ -5,21 +5,32 @@ var path   = require('path')
 var tmpdir = osenv.tmpdir()
 var pull   = require('pull-stream')
 var tape   = require('tape')
+var rimraf = require('rimraf')
 
-var db = GoatDB(path.join(tmpdir, 'test-goatdb1'))
+var dir = path.join(tmpdir, 'test-goatdb1')
+
+
+var input = []
+
+var db = GoatDB(dir)
 
 tape('simple', function (t) {
 
-  db.open(function (err) {
+  rimraf(dir, function (err) {
     if(err) throw err
-    console.log('opened')
-    var key = 'foo'
-    db.put(key, ''+ new Date(), function (err) {
+    db.open(function (err) {
       if(err) throw err
-      db.get(key, function (err, value) {
+      console.log('opened')
+      var key = 'foo'
+      var value = new Date().toISOString()
+      input.push({key: key, value: value})
+      db.put(key, value, function (err) {
         if(err) throw err
-        console.log(key, '=>', value)
-        t.end()
+        db.get(key, function (err, value) {
+          if(err) throw err
+          console.log(key, '=>', value)
+          t.end()
+        })
       })
     })
   })
@@ -28,23 +39,31 @@ tape('simple', function (t) {
 
 tape('bulkload', function (t) {
 
-  db.open(function (err) {
-    if(err) throw err
 
-    pull(
-      pull.count(999),
-      pull.map(function (e) {
-        return {key: '#' + Math.random(), value: {count: e, ts: Date.now()}}
-      }),
-      pull.asyncMap(function (e, cb) {
-        db.put(e.key, e.value, cb)
-      }),
-      pull.drain(null, function (err) {
-        if(err) throw err
-        console.log('DONE')
-      })
-    )
-
-  })
+  pull(
+    pull.count(256),
+    pull.map(function (e) {
+      return {key: '#' + Math.random(), value: {count: e, ts: Date.now()}}
+    }),
+    pull.asyncMap(function (e, cb) {
+      input.push(e)
+      db.put(e.key, e.value, cb)
+    }),
+    pull.drain(null, function (err) {
+      if(err) throw err
+      console.log('DONE')
+      pull(
+        db.createReadStream(),
+        pull.collect(function (err, actual) {
+          if(err) throw err
+          input.sort(function (a, b) {
+            return a.key < b.key ? -1 : a.key > b.key ? 1 : 0
+          })
+          t.deepEqual(actual, input)
+          t.end()
+        })
+      )
+    })
+  )
 
 })
